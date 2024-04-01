a13bec091cd5de76d222f263dd0de42c39b41f5b
Adam Young <adam.p.young@gmail.com>
2024-03-31 22:04:28 -0400

    more

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as serviceWorker from './serviceWorkerRegistration';
 
 ReactDOM.render(
@@ -11,4 +12,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
+serviceWorkerRegistration.register();
